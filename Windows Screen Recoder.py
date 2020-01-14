72efda95ae876edaa7128a8237b4a89b8593a7cc
Arbaz Khan <49393716+Arbazkhan4712@users.noreply.github.com>
2020-01-14 19:56:12 +0530

    Add files via upload

---
+++ Windows Screen Recoder.py
@@ -0,0 +1,29 @@
+import numpy as np 
+import cv2 
+# for windows, mac users
+from PIL import ImageGrab
+# for linux users
+#import pyscreenshot as ImageGrab
+
+# four character code object for video writer
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# video writer object
+out = cv2.VideoWriter("output.avi", fourcc, 5.0, (1366, 768))
+
+while True:
+	# capture computer screen
+	img = ImageGrab.grab()
+	# convert image to numpy array
+	img_np = np.array(img)
+	# convert color space from BGR to RGB
+	frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+	# show image on OpenCV frame
+	cv2.imshow("Screen", frame)
+	# write frame to video writer
+	out.write(frame)
+  
+	if cv2.waitKey(1) == 27:
+		break
+
+out.release()
+cv2.destroyAllWindows()
